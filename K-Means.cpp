8a505dac7b10939a99a858b949c6651ce569b131
String Max <mr.tens@foxmail.com>
2022-01-21 17:52:36 +0800

    Add some methods.

--- K-Means.cpp
+++ K-Means.cpp
@@ -3,24 +3,44 @@
 #include <vector>
 
 class KMeans {
-    typedef     unsigned long long      Data;
+    typedef     double                    Data;
     typedef     std::vector <Data>      Vector;
     typedef     std::vector <Vector>    DataSet;
-    typedef     std::vector <Vector>    Cluster;
+    typedef     std::vector <Vector>    Clusters;
     typedef     std::vector <size_t>    Tag;
 public:
     KMeans() = default;
-    Cluster cluster(const DataSet& dataset, Tag &tag, size_t k);
+    Clusters cluster(const DataSet &dataset, Tag &tag, size_t k);
 private:
-    size_t getDistance(const Vector &a, const Vector &b);
+    double getDistance(const Vector &a, const Vector &b);
+    double getWCSS(const DataSet &dataset, const Tag &tag, const Clusters &clusters);
+    void assignment(const DataSet &dataset, Tag &tag, const Clusters &clusters);
+    void update(const DataSet &dataset, const Tag &tag, Clusters &clusters);
 };
 
 // K-Means.cpp
 // #include "K-Means.h"
 #include <ctime>
 #include <cstdlib>
+#include <set>
 
-KMeans::Cluster KMeans::cluster(const DataSet& dataset, Tag &tag, size_t k) {
+KMeans::Clusters KMeans::cluster(const DataSet &dataset, Tag &tag, size_t k) {
+    /* XXX: Randomly select k vectors as the initial cluster. */
+    /* But this method may cause timeout, I think. */
     srand(time(NULL));
-    /* TODO: Randomly select k vectors as the initial cluster. */
+    std::set <size_t> selector;
+    while(selector.size() < k) {
+        selector.insert(rand() % dataset.size());
+    }
+    
+    Clusters clusters;
+    for(auto index: selector) {
+        clusters.push_back(dataset[index]);
+    }
+
+    size_t iteration_count = 100, last_wcss;
+    while(0 != iteration_count--) {
+        assignment(dataset, tag, clusters);
+        update(dataset, tag, clusters);
+    }
 }
