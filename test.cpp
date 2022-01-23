5223792079644548e3f08d2155318f623da84563
String Max <mr.tens@foxmail.com>
2022-01-23 13:21:40 +0800

    Small test, find bugs.

--- test.cpp
+++ test.cpp
@@ -1,1 +1,22 @@
-/* TODO: Write a test for the first version. */
+/* DONE: Write a test for the first version. */
+
+#include "K-Means.cpp"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    vector <vector <double>> dataset, clusters;
+    for(int i = 0; i < 10; i++) {
+        dataset.push_back({ 1.0 * i, 1.0 * i });
+    }
+    KMeans kmeans;
+    vector <size_t> tag(10);
+    clusters = kmeans.cluster(dataset, tag, 3);
+    for(auto &vec: clusters) {
+        for(auto x: vec) {
+            cout << x << ' ';
+        }
+        cout << endl;
+    }
+}
